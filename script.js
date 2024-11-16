830761c5ada98b1bfde886e6427512d5aaded563
Gustavo Canepae <gustavociscotto@gmail.com>
2024-11-16 14:30:42 -0300

    Adicionando melhorias

--- script.js
+++ script.js
@@ -8,15 +8,80 @@ form.addEventListener("submit", (event) => {
     event.preventDefault()
 
 
+    const newItem = {
+        id: new Date().getTime(),
+        item_name: item.value,
+    }
 
-    if (input.value !== "") {
-        manageItem()
+     console.log(newItem)
+     itemAdd(newItem)
+
+
+
+
+    
+
+    function itemAdd (newItem) {
+
+        try {
+
+
+        // Criando o item
+        const listItem = document.createElement("li")
+        listItem.classList.add("li")
+        
+
+
+        // Criando o conteúdo do item
+        const itemContent = document.createElement("span")
+        itemContent.textContent = newItem.item_name
+
+        // Criando a data que foi o item foi adiciona na lista
+        
+        
+
+
+        // Criando o removebutton
+        const removebutton = document.createElement("button")
+        removebutton.innerHTML = `<img src="assets/icons/Frame-3.svg" alt="">`
+
+
+        // Adicionando os elementos no item e na lista
+        listItem.append(itemContent)
+        listItem.append(removebutton)
+        listaItem.append(listItem)
+
+
+
+        removebutton.addEventListener("click", (event) => {
+            event.preventDefault()
+            alert("Item removido com sucesso!")
+            listItem.remove()
+            
+        })
+        
+        
     
     
-    } else {
-        alert("O campo está vazio")
+        } catch (error) {
+            alert("Não foi possivel executar a ação")
+            
+        }
+
+        
+
+
+
+
 
     }
+
+
+    
+
+
+
+    
 
 
     
@@ -37,66 +102,11 @@ form.addEventListener("submit", (event) => {
 
 
 
-function manageItem () {
-
-    // Adicionar item
-    
-    const NovoItem = document.createElement("li")
-
-    NovoItem.classList.add("li")
-
-    const NomeItem = document.createElement("span")
-
-    NomeItem.textContent = input.value
-
-    
-   
-
-   
 
 
-                    
-    NovoItem.append(NomeItem)
-
-    listaItem.append(NovoItem)
-    console.log(NovoItem)
 
 
-    const removebutton = document.createElement("button")
-    removebutton.innerHTML= `<img src="assets/icons/Frame-3.svg" alt="">`
-
-    NovoItem.append(removebutton)
-
-
-    input.value = ""
 
     
 
 
-    // Remover item
-
-
-   
-
-    removebutton.addEventListener("click", (event) => {
-        event.preventDefault()
-        console.log("Clique!")
-
-
-        NovoItem.remove(listaItem)
-
-
-
-
-
-
-    })
-
-    
-
-
-
-   
-    
-
-}
